53e367d9327cd304a709792a1af953a3b57230f4
unknown <chikajethro1616@gmail.com>
2024-07-30 00:04:36 +0200

    changes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import morgan from "morgan";
 import compression from "compression";
 import router from "./products/fetch-products-byUser-Id";
 
+
+
 const app: Application = express();
 const PORT = 2000;
 
@@ -12,7 +14,15 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(compression());
 
-app.use('/src',router);
+app.use('/products',router);
+
+app.post('/product/user/create', (req, res) => {
+  const { name } = req.body;
+  // Handle the creation logic here
+  res.status(201).send({ message: `Product ${name} created successfully` });
+});
+
+
 app.get("/", (_req: Request, res: Response) => {
   res.send("Server is On");
 });
